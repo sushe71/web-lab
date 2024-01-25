a9501129e4b7b66a07b0117c28b43fcbf9291c39
sushe71 <157725214+sushe71@users.noreply.github.com>
2024-01-25 21:35:45 +0545

    Add files via upload

---
+++ lab3.js
@@ -0,0 +1,9 @@
+function btn_actn() {
+    var username = document.getElementById("id").value;
+    var password = document.getElementById("pw").value;
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    window.location.href = "lab3_detail.html";
+}
